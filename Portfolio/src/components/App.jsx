3438d44dcf825564758fd865d373fc876e2097be
Vedv2046 <vharambleved@gmail.com>
2026-07-13 14:34:03 +0530

    installed Anaylytics dependencies

--- Portfolio/src/components/App.jsx
+++ Portfolio/src/components/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 import Lenis from 'lenis';
 import { SiReact, SiNodedotjs, SiJavascript, SiGit, SiPostgresql, SiExpress } from 'react-icons/si';
 
